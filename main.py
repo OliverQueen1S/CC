1fa23cf32e5a4f6bf1cb758cf508161e0bfacc37
Rimuru Tempest <98388995+rimurx@users.noreply.github.com>
2022-02-02 17:57:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import time
 import random
 print("open source soon bro")
-time.sleep(3)
+time.sleep(5)
 print("Thanks For Trust")
 #Below will make it so the window doesn't auto-close.
 input()
